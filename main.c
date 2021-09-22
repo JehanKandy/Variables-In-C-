d2d4ff2910ee77f47590693ec67138f7fb521464
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-09-22 14:45:21 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char pName[] = "Weerasuriya";
+    int pAge = 25;
+    printf("Veriable in C \n");
+    printf("My Name is %s\n", pName);
+    printf("I am %d years old \n", pAge);
+
+
+
+    return 0;
+}
